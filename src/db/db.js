1bdcbec27b9ed8786af9b38986c75f7c47212aba
LeonardJG7 <115773781+LeonardJG7@users.noreply.github.com>
2023-05-23 01:54:10 -0400

    Add files via upload

---
+++ src/db/db.js
@@ -0,0 +1,10 @@
+import { Sequelize } from "sequelize";
+import { config } from "dotenv";
+
+config();
+
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres'
+});
